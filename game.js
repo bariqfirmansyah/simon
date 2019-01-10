2af971fb58def3322a4a49cf01975b3171d0572f
bariqfirmansyah <bariqsufif@gmail.com>
2019-01-10 14:40:03 +0700

    Add files via upload

--- game.js
+++ game.js
@@ -44,6 +44,7 @@ function nextSequence() {
   gamePattern.push(randomChosenColour);
   $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound(randomChosenColour);
+  animatePress(randomChosenColour);
   level++;
   $("#level-title").text("level "+level)
 
